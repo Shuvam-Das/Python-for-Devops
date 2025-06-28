9a4c20dc9de9c57e6056906ea885b817bb27be6f
Shuvam Das <36703136+Shuvam-Das@users.noreply.github.com>
2025-06-28 12:44:20 +0000

    Newly added module code

---
+++ Day-04/calculator-new.py
@@ -0,0 +1,23 @@
+num1 = 10
+num2 = 5
+
+def addition():
+    add = num1 + num2
+    print("Value of addition:", add)
+
+def subtraction():
+      sub = num1 - num2
+      print("Subtraction:", sub)
+
+def multiplication():
+       mul = num1 * num2
+       print("Multiplication:", mul)
+
+def division():
+       div = num1 / num2
+       print("Division:", div)
+
+addition()
+subtraction()
+multiplication()
+division()
---
+++ Day-04/calculator.py
@@ -0,0 +1,14 @@
+num1 = 10
+num2 = 5
+
+addition = num1 + num2
+print( "value of addition:", addition)
+
+subtraction = num1 - num2
+print("Subtraction:", subtraction)
+
+multiplication = num1 * num2
+print("Multiplication:", multiplication)
+
+division = num1 / num2
+print("Division:", division)
